42b888c6001c0d90739ecb521c26d985a467e836
kinten108101 <kinten108101@protonmail.com>
2023-10-11 23:07:48 +0700

    new exception type

--- Utils/utils.h
+++ Utils/utils.h
@@ -12,6 +12,13 @@ namespace utils {
 		NotImplementedException(std::string method_name) : std::logic_error("Method " + method_name + " not implemented.") {}
 	};
 
+	class NullPointerException : public std::logic_error {
+	protected:
+	    std::string msg = "";
+	public:
+	    NullPointerException(std::string ptr_name): std::logic_error("Pointer \"" + ptr_name + "\" is null.") {}
+	};
+
 	extern int assert(std::function<int()> predicate);
 	extern int runtest(std::string label, std::vector<std::function<int()>> tests);
 }
